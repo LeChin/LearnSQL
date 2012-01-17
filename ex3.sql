4d353e7abbfdf9d88e1782653cc59eb6c5045429
Nichole Barrett <elohcin@gmail.com>
2012-01-17 15:16:18 -0800

    first example files

---
+++ ex3.sql
@@ -0,0 +1,9 @@
+insert into person (id, first_name, last_name, age)
+	values (0, "Nichole", "Barrett", 23);
+
+insert into pet (id, name, breed, age, dead)
+	values (0, "Oso", "Kitten Man", 7, 0);
+
+insert into pet (id, name, breed, age, dead)
+	values (1, "Waffle Pancake Syrup", "Kitten Man", 7, 0);
+
